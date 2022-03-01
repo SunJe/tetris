221a7889ddfc14f298958129d83d3f731f9bc1f2
SunJe <100393011+SunJe@users.noreply.github.com>
2022-03-01 14:32:18 +0900

    Add files via upload

--- js/tetris.js
+++ js/tetris.js
@@ -1,7 +1,9 @@
 
-import BLOCKS from "./blocks.js"
 // DOM
 const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".game-text");
+const scoreDisplay = document.querySelector(".score");
+const restartButton = document.querySelector(".game-text > button")
 // Setting
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
@@ -12,9 +14,55 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
+const BLOCKS = {
+  tree: [
+    [[2,1],[0,1],[1,0],[1,1]],
+    [[1,2],[0,1],[1,0],[1,1]],
+    [[2,1],[1,2],[0,1],[1,1]],
+    [[2,1],[1,2],[1,0],[1,1]],
+  ],
+  square: [
+    [[0,0],[0,1],[1,0],[1,1]],
+    [[0,0],[0,1],[1,0],[1,1]],
+    [[0,0],[0,1],[1,0],[1,1]],
+    [[0,0],[0,1],[1,0],[1,1]],
+  ],
+  bar: [
+    [[0,0],[1,0],[2,0],[3,0]],
+    [[2,0],[2,1],[2,2],[2,3]],
+    [[0,0],[1,0],[2,0],[3,0]],
+    [[2,0],[2,1],[2,2],[2,3]],
+  ],
+  elLeft: [
+    [[0,0],[0,1],[1,1],[2,1]],
+    [[2,0],[2,1],[2,2],[1,2]],
+    [[0,0],[1,0],[2,0],[2,1]],
+    [[0,0],[0,1],[0,2],[1,0]],
+  ],
+  elRight: [
+    [[0,1],[1,1],[2,1],[2,0]],
+    [[1,0],[1,1],[1,2],[0,0]],
+    [[0,0],[1,0],[2,0],[0,1]],
+    [[0,0],[0,1],[0,2],[1,2]],
+  ],
+  zeeLeft: [
+    [[0,0],[0,1],[1,1],[1,2]],
+    [[1,0],[1,1],[0,1],[2,0]],
+    [[0,0],[0,1],[1,1],[1,2]],
+    [[1,0],[1,1],[0,1],[2,0]],
+  ],
+  zeeRight: [
+    [[0,1],[0,2],[1,0],[1,1]],
+    [[0,0],[1,0],[1,1],[2,1]],
+    [[0,1],[0,2],[1,0],[1,1]],
+    [[0,0],[1,0],[1,1],[2,1]],
+  ]
+}
+
+
 
 const movingItem = {
-  type:"tree",
+  type:"",
   direction: 0,
   top: 0,
   left: 0,
@@ -28,7 +76,7 @@ function init(){
   for(let i =0; i<GAME_ROWS; i++){
     prependNewLine()
   }
-  renderBlocks()
+  generateNewBlock()
 }
 
 function prependNewLine(){
@@ -58,8 +106,12 @@ function renderBlocks(moveType=""){
       target.classList.add(type,"moving")
     } else {
       tempMovingItem={...movingItem}
+      if(moveType === 'retry'){
+        clearInterval(downInterval)
+        showGameoverText()
+      }
       setTimeout(()=>{
-        renderBlocks()
+        renderBlocks('retry')
         if(moveType ==="top"){
           seizeBlock();
         }
@@ -80,10 +132,37 @@ function seizeBlock(){
     moving.classList.remove("moving");
     moving.classList.add("seized");
   })
-  generateNewBlock()
+  checkMatch()
+}
+
+function checkMatch(){
+  const childNodes = playground.childNodes;
+  childNodes.forEach(child=>{
+    let matched = true;
+    child.children[0].childNodes.forEach(li=>{
+      if(!li.classList.contains("seized")){
+        matched = false
+      }
+    })
+    if(matched){
+      child.remove();
+      prependNewLine()
+      score++;
+      scoreDisplay.innerText = score;
+    }
+  })
+    generateNewBlock()
 }
 
 function generateNewBlock(){
+  clearInterval(downInterval);
+  downInterval = setInterval(()=>{
+    moveBlock('top',1)
+  },duration)
+
+  const blockArray = Object.entries(BLOCKS);
+  const randomIndex = Math.floor(Math.random() * blockArray.length)
+  movingItem.type = blockArray[randomIndex][0];
   movingItem.top = 0;
   movingItem.left = 3;
   movingItem.direction = 0;
@@ -107,6 +186,17 @@ function changeDirection(){
   direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction +=1;
   renderBlocks()
 }
+
+function dropBlock(){
+  clearInterval(downInterval);
+  downInterval = setInterval(() => {
+    moveBlock('top',1)
+  },10)
+}
+
+function showGameoverText(){
+  gameText.style.display = "flex"
+}
 //event handling
 document.addEventListener("keydown",e => {
   switch(e.keyCode){
@@ -122,8 +212,19 @@ document.addEventListener("keydown",e => {
     case 38:
       changeDirection();
       break;
+    case 32:
+      dropBlock();
+      break;
     default:
       break;
   }
   //console.log(e)
+})
+
+restartButton.addEventListener("click",() =>{
+  playground.innerHTML = "";
+  gameText.style.display = "none";
+  score = 0;
+  scoreDisplay.innerText = score;
+  init()
 })
